754ccd585a2ac4351ff37a32ca4ace75a57d3bc1
JacobFinn <Jacob.Finn102@gmail.com>
2018-09-11 09:55:59 -0500

    Initial Commit

---
+++ DeckGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DeckGame
+//
+//  Created by Jacob Finn on 9/11/18.
+//  Copyright © 2018 Jacob Finn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
